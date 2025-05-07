500b19a2c6cad1a3cdb606e2e701c8bc36c20f92
chika-1114777@g.ecc.u-tokyo.ac.jp <chika-kiichi>
2025-05-07 14:19:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,128 @@
+import { useEffect, useState } from "react";
+
+export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const tasks: Todo[] = JSON.parse(localStorage.getItem("tasks") || "[]");
+    return tasks;
+  });
+  const [filter, setFilter] = useState<Filter>("all");
+
+  const handleSubmit = () => {
+    if (!text) return;
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
+    };
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
+        }
+      });
+      return newTodos;
+    });
+  };
+  const handleFilter = (filter: Filter) => {
+    setFilter(filter);
+  };
+  const handleEmpty = () => {
+    // シャローコピーで事足りる
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };
+  const filteredTodos = todos.filter((todo) => {
+    // filter ステートの値に応じて異なる内容の配列を返す
+    switch (filter) {
+      case "all":
+        // 削除されていないもの
+        return !todo.removed;
+      case "checked":
+        // 完了済 **かつ** 削除されていないもの
+        return todo.checked && !todo.removed;
+      case "unchecked":
+        // 未完了 **かつ** 削除されていないもの
+        return !todo.checked && !todo.removed;
+      case "removed":
+        // 削除済みのもの
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(todos));
+  }, [todos]);
+  return (
+    <div>
+      <select
+        defaultValue="all"
+        onChange={(e) => handleFilter(e.target.value as Filter)}
+      >
+        <option value="all">すべてのタスク</option>
+        <option value="checked">完了したタスク</option>
+        <option value="unchecked">現在のタスク</option>
+        <option value="removed">ごみ箱</option>
+      </select>
+      {filter === "removed" ? (
+        <button
+          onClick={handleEmpty}
+          disabled={todos.filter((todo) => todo.removed).length === 0}
+        >
+          ごみ箱を空にする
+        </button>
+      ) : (
+        filter !== "checked" && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <input type="text" value={text} onChange={(e) => handleChange(e)} />
+            <input type="submit" value="追加" onSubmit={handleSubmit} />
+          </form>
+        )
+      )}
+      <ul>
+        {filteredTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                disabled={todo.removed}
+                checked={todo.checked}
+                onChange={() => handleTodo(todo.id, "checked", !todo.checked)}
+              />
+              <input
+                type="text"
+                disabled={todo.checked || todo.removed}
+                value={todo.value}
+                onChange={(e) => handleTodo(todo.id, "value", e.target.value)}
+              />
+              <button
+                onClick={() => handleTodo(todo.id, "removed", !todo.removed)}
+              >
+                {todo.removed ? "復元" : "削除"}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
